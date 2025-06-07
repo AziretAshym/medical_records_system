98a38f92997bd645d7ff746b9923dcb3ad91cc9c
Ashymov Aziret <aziretashym@gmail.com>
2025-06-07 13:55:16 +0600

    [Client] Connected global state, added variable for requests.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import { Provider } from 'react-redux';
+import { persistor, store } from './app/store.ts';
+import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
+import { ToastContainer } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
+import 'normalize.css';
+import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
+createRoot(document.getElementById("root")!).render(
+    <Provider store={store}>
+        <PersistGate persistor={persistor} />
+        <ToastContainer position={"top-center"} />
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
+);
