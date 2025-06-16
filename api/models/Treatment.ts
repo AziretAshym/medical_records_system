aa178b4149b581ac89ff8506a1b38d7b2f57f1da
Ashymov Aziret <aziretashym@gmail.com>
2025-06-16 16:19:02 +0600

    [Api] Created model Counter, and remade Treatment and MedicalRecord models with number generation.

--- api/models/Treatment.ts
+++ api/models/Treatment.ts
@@ -1,5 +1,4 @@
 import mongoose, { HydratedDocument, Model } from "mongoose";
-import Counter from "./Counter";
 
 export interface TreatmentFields {
     medicalRecord: mongoose.Types.ObjectId;
@@ -15,7 +14,6 @@ export interface TreatmentFields {
     status: 'scheduled' | 'in-progress' | 'completed' | 'canceled';
     assignedBy: mongoose.Types.ObjectId;
     updatedBy: mongoose.Types.ObjectId;
-    treatmentNumber: string;
 }
 
 type TreatmentModel = Model<TreatmentFields, {}>;
@@ -61,32 +59,11 @@ const TreatmentSchema = new Schema<HydratedDocument<TreatmentFields>, TreatmentM
         ref: 'User',
         required: true,
     },
-    treatmentNumber: {
-        type: String,
-        unique: true,
-    },
 }, { timestamps: true });
 
 TreatmentSchema.index({ patient: 1, startDate: -1 });
 TreatmentSchema.index({ medicalRecord: 1 });
 TreatmentSchema.index({ status: 1 });
-
-TreatmentSchema.pre('save', async function (next) {
-    if (!this.isNew) return next();
-
-    try {
-        const counter = await Counter.findOneAndUpdate(
-            { name: 'treatment' },
-            { $inc: { seq: 1 } },
-            { new: true, upsert: true }
-        );
-
-        this.treatmentNumber = `TRT-${counter.seq}`;
-        next();
-    } catch (err) {
-        next(err as Error);
-    }
-});
 
 const Treatment = mongoose.model("Treatment", TreatmentSchema);
 export default Treatment;
